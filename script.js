fed16b76f575f0f8195765116e5bd3a368c411b9
Vkram <mahakram35@gmail.com>
2024-07-07 15:19:42 +0300

    push

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     const uploadButton = document.getElementById('upload-button');
     const imageUploadInput = document.getElementById('image-upload');
-    const logoUploadInput = document.getElementById('logo-upload');
     const applyLogoButton = document.getElementById('apply-logo-button');
+    const applyResizeButton = document.getElementById('apply-resize-button');
+    const applyLogoResizeButton = document.getElementById('apply-logo-resize-button');
     const downloadButton = document.getElementById('download-button');
     const imageGallery = document.getElementById('image-gallery');
     const logoPositionSelect = document.getElementById('logo-position');
@@ -11,8 +12,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const resizeEnableCheckbox = document.getElementById('resize-enable');
     const resizeWidthInput = document.getElementById('resize-width');
     const resizeHeightInput = document.getElementById('resize-height');
+    const logoPositionResizeSelect = document.getElementById('logo-position-resize');
+    const logoWidthResizeInput = document.getElementById('logo-width-resize');
+    const logoHeightResizeInput = document.getElementById('logo-height-resize');
     const overlay = document.getElementById('overlay');
     let logoSrc = '';
+    let logoResizeSrc = '';
 
     uploadButton.addEventListener('click', function() {
         const files = imageUploadInput.files;
@@ -28,8 +33,8 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    logoUploadInput.addEventListener('change', function() {
-        const file = logoUploadInput.files[0];
+    document.getElementById('logo-upload').addEventListener('change', function() {
+        const file = this.files[0];
         if (file) {
             const reader = new FileReader();
             reader.onload = function(e) {
@@ -39,26 +44,57 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    document.getElementById('logo-upload-resize').addEventListener('change', function() {
+        const file = this.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                logoResizeSrc = e.target.result;
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+
+    applyResizeButton.addEventListener('click', function() {
+        const images = document.querySelectorAll('.image-preview img');
+        showOverlay();
+        const tasks = Array.from(images).map(img => {
+            const container = img.parentElement;
+            const cropWidth = parseInt(resizeWidthInput.value);
+            const cropHeight = parseInt(resizeHeightInput.value);
+            const shouldResize = resizeEnableCheckbox.checked;
+
+            if (shouldResize) {
+                return cropImage(img, cropWidth, cropHeight).then(croppedImg => {
+                    container.replaceChild(croppedImg, img);
+                });
+            }
+        });
+
+        Promise.all(tasks).then(() => hideOverlay());
+    });
+
     applyLogoButton.addEventListener('click', function() {
         const images = document.querySelectorAll('.image-preview img');
         showOverlay();
         const tasks = Array.from(images).map(img => {
             const container = img.parentElement;
-            const resizeWidth = resizeWidthInput.value;
-            const resizeHeight = resizeHeightInput.value;
-            const shouldResize = resizeEnableCheckbox.checked;
+            return applyLogoToImage(img).then(finalImg => {
+                container.replaceChild(finalImg, img);
+            });
+        });
 
-            if (shouldResize) {
-                return resizeImage(img, resizeWidth, resizeHeight).then(resizedImg => {
-                    return applyLogoToImage(resizedImg).then(finalImg => {
-                        container.replaceChild(finalImg, img);
-                    });
-                });
-            } else {
-                return applyLogoToImage(img).then(finalImg => {
-                    container.replaceChild(finalImg, img);
-                });
-            }
+        Promise.all(tasks).then(() => hideOverlay());
+    });
+
+    applyLogoResizeButton.addEventListener('click', function() {
+        const images = document.querySelectorAll('.image-preview img');
+        showOverlay();
+        const tasks = Array.from(images).map(img => {
+            const container = img.parentElement;
+            return applyLogoToImage(img, true).then(finalImg => {
+                container.replaceChild(finalImg, img);
+            });
         });
 
         Promise.all(tasks).then(() => hideOverlay());
@@ -67,8 +103,10 @@ document.addEventListener('DOMContentLoaded', function() {
     downloadButton.addEventListener('click', function() {
         const images = document.querySelectorAll('.image-preview img');
         showOverlay();
+        let counter = 1;
         images.forEach(img => {
-            downloadImage(img);
+            downloadImage(img, counter);
+            counter++;
         });
         hideOverlay();
     });
@@ -92,46 +130,35 @@ document.addEventListener('DOMContentLoaded', function() {
         imageGallery.appendChild(div);
     }
 
-    function resizeImage(img, maxWidth, maxHeight) {
+    function cropImage(img, width, height) {
         return new Promise((resolve) => {
             const canvas = document.createElement('canvas');
             const ctx = canvas.getContext('2d');
             const imgWidth = img.naturalWidth;
             const imgHeight = img.naturalHeight;
 
-            let width = imgWidth;
-            let height = imgHeight;
-
-            if (width > maxWidth || height > maxHeight) {
-                const aspectRatio = width / height;
-
-                if (width > height) {
-                    width = maxWidth;
-                    height = Math.round(maxWidth / aspectRatio);
-                } else {
-                    height = maxHeight;
-                    width = Math.round(maxHeight * aspectRatio);
-                }
-            }
+            // Calculate the cropping area
+            let offsetX = (imgWidth - width) / 2;
+            let offsetY = (imgHeight - height) / 2;
 
             canvas.width = width;
             canvas.height = height;
-            ctx.drawImage(img, 0, 0, width, height);
+            ctx.drawImage(img, offsetX, offsetY, width, height, 0, 0, width, height);
 
-            const resizedImg = new Image();
-            resizedImg.src = canvas.toDataURL();
-            resizedImg.onload = function() {
-                resolve(resizedImg);
+            const croppedImg = new Image();
+            croppedImg.src = canvas.toDataURL();
+            croppedImg.onload = function() {
+                resolve(croppedImg);
             };
         });
     }
 
-    function applyLogoToImage(img) {
+    function applyLogoToImage(img, isResizeTab = false) {
         return new Promise((resolve) => {
             const canvas = document.createElement('canvas');
             const ctx = canvas.getContext('2d');
             const logo = new Image();
-            logo.src = logoSrc;
+            logo.src = isResizeTab ? logoResizeSrc : logoSrc;
 
             logo.onload = function() {
                 const imgWidth = img.naturalWidth;
@@ -141,9 +168,9 @@ document.addEventListener('DOMContentLoaded', function() {
                 canvas.height = imgHeight;
                 ctx.drawImage(img, 0, 0, imgWidth, imgHeight);
 
-                const logoWidth = parseInt(logoWidthInput.value);
-                const logoHeight = parseInt(logoHeightInput.value);
-                const position = logoPositionSelect.value;
+                const logoWidth = parseInt(isResizeTab ? logoWidthResizeInput.value : logoWidthInput.value);
+                const logoHeight = parseInt(isResizeTab ? logoHeightResizeInput.value : logoHeightInput.value);
+                const position = isResizeTab ? logoPositionResizeSelect.value : logoPositionSelect.value;
                 const offset = 40;
                 let x = 0;
                 let y = 0;
@@ -178,10 +205,10 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    function downloadImage(img) {
+    function downloadImage(img, counter) {
         const a = document.createElement('a');
         a.href = img.src;
-        a.download = 'image_with_logo.png';
+        a.download = `image_with_logo_${counter}.png`;
         document.body.appendChild(a);
         a.click();
         document.body.removeChild(a);
@@ -195,3 +222,20 @@ document.addEventListener('DOMContentLoaded', function() {
         overlay.style.visibility = 'hidden';
     }
 });
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Open the first tab by default
+document.querySelector('.tab button').click();
